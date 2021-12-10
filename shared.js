f875814daae7818217b81a200f6ac3ef1085d270
theo82 <theoaristi53@gmail.com>
2021-12-10 09:27:43 +0200

    Adding a Side Navigation

--- shared.js
+++ shared.js
@@ -1,5 +1,6 @@
 var backdrop = document.querySelector('.backdrop')
 var modal = document.querySelector('.modal')
+var modalNoButton = document.querySelector('.modal__action--negative')
 var selectPlanButtons = document.querySelectorAll('.plan button')
 // console.dir(backdrop)
 // console.dir(selectPlanButton)
@@ -10,4 +11,12 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     modal.style.display = 'block'
     backdrop.style.display = 'block'
   })
+}
+
+backdrop.addEventListener('click', closeModal)
+
+modalNoButton.addEventListener('click', closeModal)
+
+function closeModal() {
+  ;(backdrop.style.display = 'none'), (modal.style.display = 'none')
 }
