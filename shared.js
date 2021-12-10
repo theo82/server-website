8df74c81520cff1d20e2c93650e68ca2c9ee1bc8
theo82 <theoaristi53@gmail.com>
2021-12-10 10:06:11 +0200

    Opening and Closing the Hamburger button

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop')
 var modal = document.querySelector('.modal')
 var modalNoButton = document.querySelector('.modal__action--negative')
 var selectPlanButtons = document.querySelectorAll('.plan button')
+var toggleButton = document.querySelector('.toggle-button')
+var mobileNav = document.querySelector('.mobile-nav')
 // console.dir(backdrop)
 // console.dir(selectPlanButton)
 console.dir(modal)
@@ -13,10 +15,18 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
   })
 }
 
-backdrop.addEventListener('click', closeModal)
+backdrop.addEventListener('click', function () {
+  mobileNav.style.display = 'none'
+  closeModal()
+})
 
 modalNoButton.addEventListener('click', closeModal)
 
 function closeModal() {
   ;(backdrop.style.display = 'none'), (modal.style.display = 'none')
 }
+
+toggleButton.addEventListener('click', function () {
+  mobileNav.style.display = 'block'
+  backdrop.style.display = 'block'
+})
